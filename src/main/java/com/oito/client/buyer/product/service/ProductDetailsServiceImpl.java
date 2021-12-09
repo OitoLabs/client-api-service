81748341d16b1c35d1abbec77ba1656e9a104f83
anandu <anandus@scg.com>
2021-12-09 17:24:24 +0530

    pChange

--- src/main/java/com/oito/client/buyer/product/service/ProductDetailsServiceImpl.java
+++ src/main/java/com/oito/client/buyer/product/service/ProductDetailsServiceImpl.java
@@ -7,10 +7,24 @@ import com.oito.client.buyer.product.response.PdpFreeSampleResponse;
 @Service
 public class ProductDetailsServiceImpl implements ProductDetailsService {
 
-	@Override
-	public PdpFreeSampleResponse getFreeSampleDetails(String productId, String lang) {
-		System.out.println("working our API's");
-		return null;
-	}
 
+	@Override
+	public PdpFreeSampleResponse getFreeSampleDetails(final String productId, final String lang) {
+		final var resultMap = getProductDetailsMap(productId, lang);
+		throwIfNull(resultMap.get(Constants.SKU_NUMBER), () -> new ApiException(ErrorCode.INVALID_PRODUCT_ID));
+		final var freeSampleVariants = getFreeSampleVariants(resultMap);
+		final var freeSampleVariant = (VaraintDetails) resultMap.get(Constants.FREE_SAMPLE_VARIANT);
+		final var groupBy = getIfNonNull(freeSampleVariant, () -> freeSampleVariant.getTitle());
+		if (groupBy == null) {
+			return getMobileResponse(List.of(new SectionVO("", freeSampleVariants)), lang);
+		}
+		// Grouping
+		final var sections = getSections(freeSampleVariants, groupBy);
+
+		// Convert to response format
+		final var sectionList = sections.entrySet().stream()
+				.map(entry -> new SectionVO(entry.getKey(), entry.getValue())).collect(Collectors.toList());
+
+		return getMobileResponse(sectionList, lang);
+	}
 }
